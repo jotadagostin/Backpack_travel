675cb99c0db8c6a5d0386e79e6cfb2317d395a92
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-02-15 22:46:00 +0100

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,79 +1,83 @@
-const form = document.getElementById("novoItem")
-const lista = document.getElementById("lista")
-const itens = JSON.parse(localStorage.getItem("itens")) || []
+const form = document.getElementById("novoItem");
+const lista = document.getElementById("lista");
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
 
-itens.forEach( (elemento) => {
-    criaElemento(elemento)
-} )
+itens.forEach((elemento) => {
+  criaElemento(elemento);
+});
 
 form.addEventListener("submit", (evento) => {
-    evento.preventDefault()
+  evento.preventDefault();
 
-    const nome = evento.target.elements['nome']
-    const quantidade = evento.target.elements['quantidade']
+  const nome = evento.target.elements["nome"];
+  const quantidade = evento.target.elements["quantidade"];
 
-    const existe = itens.find( elemento => elemento.nome === nome.value )
+  const existe = itens.find((elemento) => elemento.nome === nome.value);
 
-    const itemAtual = {
-        "nome": nome.value,
-        "quantidade": quantidade.value
-    }
+  const itemAtual = {
+    "nome": nome.value,
+    "quantidade": quantidade.value,
+  };
 
-    if (existe) {
-        itemAtual.id = existe.id
-        
-        atualizaElemento(itemAtual)
+  if (existe) {
+    itemAtual.id = existe.id;
 
-        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
-    } else {
-        itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id + 1 : 0;
+    atualizaElemento(itemAtual);
 
-        criaElemento(itemAtual)
+    itens[itens.findIndex((elemento) => elemento.id === existe.id)] = itemAtual;
+  } else {
+    itemAtual.id = itens[itens.length - 1] ? itens[itens.length - 1].id + 1 : 0;
 
-        itens.push(itemAtual)
-    }
+    criaElemento(itemAtual);
 
-    localStorage.setItem("itens", JSON.stringify(itens))
+    itens.push(itemAtual);
+  }
 
-    nome.value = ""
-    quantidade.value = ""
-})
+  localStorage.setItem("itens", JSON.stringify(itens));
+
+  nome.value = "";
+  quantidade.value = "";
+});
 
 function criaElemento(item) {
-    const novoItem = document.createElement("li")
-    novoItem.classList.add("item")
+  const novoItem = document.createElement("li");
+  novoItem.classList.add("item");
 
-    const numeroItem = document.createElement("strong")
-    numeroItem.innerHTML = item.quantidade
-    numeroItem.dataset.id = item.id
-    novoItem.appendChild(numeroItem)
-    
-    novoItem.innerHTML += item.nome
+  const numeroItem = document.createElement("strong");
+  numeroItem.innerHTML = item.quantidade;
+  numeroItem.dataset.id = item.id;
+  novoItem.appendChild(numeroItem);
 
-    novoItem.appendChild(botaoDeleta(item.id))
+  novoItem.innerHTML += item.nome;
 
-    lista.appendChild(novoItem)
+  novoItem.appendChild(botaoDeleta(item.id));
+
+  lista.appendChild(novoItem);
 }
 
 function atualizaElemento(item) {
-    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+  document.querySelector("[data-id='" + item.id + "']").innerHTML =
+    item.quantidade;
 }
 
 function botaoDeleta(id) {
-    const elementoBotao = document.createElement("button")
-    elementoBotao.innerText = "X"
+  const elementoBotao = document.createElement("button");
+  elementoBotao.innerText = "X";
 
-    elementoBotao.addEventListener("click", function() {
-        deletaElemento(this.parentNode, id)
-    })
+  elementoBotao.addEventListener("click", function () {
+    deletaElemento(this.parentNode, id);
+  });
 
-    return elementoBotao
+  return elementoBotao;
 }
 
 function deletaElemento(tag, id) {
-    tag.remove()
+  tag.remove();
 
-    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
+  itens.splice(
+    itens.findIndex((elemento) => elemento.id === id),
+    1
+  );
 
-    localStorage.setItem("itens", JSON.stringify(itens))
+  localStorage.setItem("itens", JSON.stringify(itens));
 }
